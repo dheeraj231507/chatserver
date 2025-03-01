fd6cee952d2c930419f26c1881153a30304aa7c0
Dheeraj kumawat <dheerajkumawat0707@gmail.com>
2025-03-02 00:05:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import { app, server } from "./socket/socket.js";
+import express from "express";
+import { connectDB } from "./db/connection1.db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+connectDB();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+// routes
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
+
+// middlwares
+import { errorMiddleware } from "./middlewares/error.middlware.js";
+app.use(errorMiddleware);
+
+server.listen(PORT, () => {
+  console.log(`your server listening at port ${PORT}`);
+});
