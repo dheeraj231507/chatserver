45ecd014870b19d83214617779b483b08c4aa90c
Dheeraj kumawat <dheerajkumawat0707@gmail.com>
2025-03-02 00:34:59 +0530

    cors1

--- socket/socket.js
+++ socket/socket.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chatclient-steel.vercel.app/",
+    origin: "https://chatclient-steel.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
